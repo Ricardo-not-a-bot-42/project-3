861061630ff2b3a3d8a488720d665499baca8ad2
nanori <leonor.ferreira@gmail.com>
2020-06-08 09:51:51 +0100

    Non used files deleted. Js renamed to jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>The app is running!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
